8facd862662efe3f4da4e7759f730e679225f4a6
ygrybkova <yuliya@grybkova.com>
2023-11-29 11:53:44 -0600

    Update my link, add boostrap

--- src/App.js
+++ src/App.js
@@ -5,8 +5,16 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather Search Engine</h1>
-      <WeatherSearch />
+      <div className="container">
+        <h1>Weather Search Engine</h1>
+        <WeatherSearch />
+        <footer>
+          <a href="https://github.com/ygrybkova/react-app" target="_blank">
+            Open-source code
+          </a>
+          by Yuliia Grybkova
+        </footer>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import WeatherSearch from "./WeatherSearch";
+import "bootstrap/dist/css/bootstrap.css";
 
 import "./styles.css";
 
