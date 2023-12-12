70cc03b47e92ae56331f83288b379b461dfb153e
ygrybkova <yuliya@grybkova.com>
2023-12-11 18:28:11 -0600

    changed description

--- src/Weather.js
+++ src/Weather.js
@@ -45,7 +45,7 @@ export default function Weather(props) {
             <div className="col-9">
               <input
                 type="search"
-                placeholder="Enter a city.."
+                placeholder="Enter your city..."
                 className="form-control"
                 autoFocus="on"
                 onChange={handleCityChange}
